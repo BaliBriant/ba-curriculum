1f43532f1b985b90aa6e5da780d3719480b8b203
BaliBriant <balarambriant@gmail.com>
2019-11-20 06:16:01 +0530

    "Minor edits"

--- apps/dhatu.js
+++ apps/dhatu.js
@@ -24,7 +24,7 @@ function Dhatu(root, trans, cl, pres, fut, pp, abs, imp, inf) {
 // List of dhatus
 var as = new Dhatu('as', 'to be, exist', 2, 'asti', '', '', '', 'astu', '');
 as.gramLink = 'https://sanskrit.inria.fr/cgi-bin/SKT/sktconjug.cgi?lex=SH&q=as&t=VH&c=2&font=roma';
-as.defLink = ''
+as.defLink = '';
 
 var car = new Dhatu('car', 'to move, behave, act, live', 1, 'carati', 'cariṣyati', 'carita', 'caritvā', 'caratu', 'caritum ');
 
