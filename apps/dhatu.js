8d8e0520422adbd5aeb5ca790147e0836c4fd390
Balaram Briant <balarambriant@gmail.com>
2019-11-20 06:12:25 +0530

    Created dhatu.html

--- apps/dhatu.js
+++ apps/dhatu.js
@@ -10,21 +10,20 @@ function listDhatus(list) {
 
 // Define an object type for dhatus
 function Dhatu(root, trans, cl, pres, fut, pp, abs, imp, inf) {
-  this.root     = root;
-  this.trans    = trans;
-  this.cl       = cl;
-  this.pres     = pres;
-  this.fut      = fut;
-  this.pp       = pp;
-  this.abs      = abs;
-  this.imp      = imp;
-  this.inf      = inf;
+  this.root   = root;
+  this.trans  = trans;
+  this.cl     = cl;
+  this.pres   = pres;
+  this.fut    = fut;
+  this.pp     = pp;
+  this.abs    = abs;
+  this.imp    = imp;
+  this.inf    = inf;
 }
 
 // List of dhatus
 var as = new Dhatu('as', 'to be, exist', 2, 'asti', '', '', '', 'astu', '');
 as.gramLink = 'https://sanskrit.inria.fr/cgi-bin/SKT/sktconjug.cgi?lex=SH&q=as&t=VH&c=2&font=roma';
-as.defLink = ''
 
 var car = new Dhatu('car', 'to move, behave, act, live', 1, 'carati', 'cariṣyati', 'carita', 'caritvā', 'caratu', 'caritum ');
 
