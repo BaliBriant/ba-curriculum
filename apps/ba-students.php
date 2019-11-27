cf6bae91a88d509cc52eddb5b6948cda6ed1a325
Balaram Briant <30948666+BaliBriant@users.noreply.github.com>
2019-11-27 13:27:12 +0530

    Update ba-students.php

--- apps/ba-students.php
+++ apps/ba-students.php
@@ -1,7 +1,39 @@
 <?php
+/*
+Plugin Name: BA Curriculum Plugin
+Description: An application for monitoring and evaluating student progress.
+Version: 0.1
+Author: Bhaktivedanta Academy
+Author URI: http://bhaktivedantaacademy.com/
+Text Domain: ba-curriculum-plugin
+*/
 
 // Not yet functional
 
+/*
+ * BA Students post type
+ *
+ * Each student should have an individual page displaying their information,
+ * including their academic level (Primary, Secondary, or Tertiary),
+ * their ashram level (Praveshika, Upaveshika, etc.), and their exam results.
+ * For secondary students the profile must also contain their credits,
+ * completed projects, and current projects. For primary students
+ *
+ */
+
+/*
+
+Work Notes
+
+Academic level and ashram level should be implemented as taxonomies,
+since they group multiples students.
+
+Student ID, exam results, completed and currect projects, and credits
+should be saved as post meta.
+
+The projects themselves probably need to be instances of another post type.
+
+*/
 function ba_students() {
     $labels = array(
         'name'               => 'Students',
@@ -21,8 +53,8 @@ function ba_students() {
         'labels'     => $labels,
         'public'     => true,
         'supports'   => array( 'title', 'comments', 'custom-fields' );
-        'taxonomies' => array( );
-        'menu_icon'  => 'dashicons-id-alt',
+        'taxonomies' => array( 'academic-level', 'ashram-level' );
+        'menu_icon'  => 'dashicons-id-alt'
     );
     register_post_type( 'ba-students', $args );
 
